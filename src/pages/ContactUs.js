de349e95465884000d640074e3babfb415aa4a1b
André Roy <andreroy25.9@gmail.com>
2020-12-13 20:04:37 -0500

    Contact Animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,21 +1,76 @@
 import React from "react";
+import styled from "styled-components";
 
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
       style={{ background: "#fff" }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <StyledTitle>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </StyledTitle>
+      <div>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Send Us A Message</h2>
+          </StyledSocial>
+        </Hide>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Send an Email.</h2>
+          </StyledSocial>
+        </Hide>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Social Media</h2>
+          </StyledSocial>
+        </Hide>
+      </div>
+    </StyledContact>
   );
 };
+
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const StyledCircle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const StyledSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
